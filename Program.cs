4614e4066cf109b90b7e3fd856a13a7925e9a24a
q-gaillard <quentingaillard@edouardgand.fr>
2026-03-31 10:26:41 +0200

    fin exercice 5

--- Program.cs
+++ Program.cs
@@ -23,5 +23,23 @@ class Program
 
         ordi2.CalculerAnciennete();
         ordi3.CalculerAnciennete();
+
+        List<Appareil> appareils = new List<Appareil>();
+        appareils.Add(new Ordinateur("Lenovo", "ThinkPad", DateTime.Now, 15.6));
+        appareils.Add(new Smartphone("Samsung", "Galaxy S21", DateTime.Now, 3));
+        appareils.Add(new Ordinateur("Dell", "XPS 13", new DateTime(2020, 5, 15), 13.3));
+        appareils.Add(new Smartphone("Apple", "iPhone 13", DateTime.Now, 3));
+        appareils.Add(new Ordinateur("HP", "Spectre x360", new DateTime(2019, 10, 1), 14.0));
+        appareils.Add(new Smartphone("Google", "Pixel 5", new DateTime(2021, 1, 1), 2));
+        appareils.Add(new Smartphone("OPPO", "Find X3 Pro", new DateTime(2021, 3, 11), 4));
+        appareils.Add(new Ordinateur("ASUS", "ROG Zephyrus G14", new DateTime(2020, 7, 1), 14.0));
+
+        foreach (Appareil appareil in appareils)
+        {
+            appareil.Allumer();
+            appareil.AfficherType();
+            appareil.AfficherGarantie();
+            appareil.AfficherInfo();
+        }
     }
 }
